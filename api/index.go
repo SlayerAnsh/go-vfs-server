edd65204369acaf6ea6853c8fc63af715c0c75ce
Anshudhar Kumar Singh <anshudhar2001@gmail.com>
2024-07-16 21:15:26 +0530

    Rewrite all routes to api

--- api/index.go
+++ api/index.go
@@ -14,11 +14,11 @@ var App *gin.Engine
 
 func init() {
 	App = gin.Default()
-	App.GET("/api/favicon.ico", func(c *gin.Context) {
+	App.GET("/favicon.ico", func(c *gin.Context) {
 		c.String(http.StatusOK, "OK")
 		return
 	})
-	App.GET("/api/protocol/*uri", func(c *gin.Context) {
+	App.GET("/protocol/*uri", func(c *gin.Context) {
 
 		host := c.Request.Host
 		chainId := strings.Split(strings.Split(host, ":")[0], ".")[0]
